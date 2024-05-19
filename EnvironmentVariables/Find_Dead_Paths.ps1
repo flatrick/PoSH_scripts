3e470f174920815e1a8a5414d745306cf0c6be3b
Patrik <flatrick@users.noreply.github.com>
2024-05-19 16:24:20 +0200

    Update Find_Dead_Paths.ps1

--- EnvironmentVariables/Find_Dead_Paths.ps1
+++ EnvironmentVariables/Find_Dead_Paths.ps1
@@ -1,12 +1,14 @@
 $machinePaths = ([System.Environment]::GetEnvironmentVariable( 'PATH', 'Machine' )).Split(";") | Where-Object { $_ } | Select -Unique
 $userPaths = ([System.Environment]::GetEnvironmentVariable( 'PATH', 'Machine' )).Split(";") | Where-Object { $_ } | Select -Unique
 
+Write-Host "Checking Machine environment variables for dead paths"
 foreach ($pathToTest in $machinePaths) {
   if (-Not (Test-Path $pathToTest) {
     Write-Host "MISSING! -> : $($pathtotest)"
   }
 }
 
+Write-Host "`tChecking User environment variables for dead paths"
 foreach ($pathToTest in $userPaths) {
   if (-Not (Test-Path $pathToTest) {
     Write-Host "MISSING! -> : $($pathtotest)"
